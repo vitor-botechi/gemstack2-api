11cab7700ab9e2608de9b9981ee6d5a42cce7967
Vitor Botechi <vbotechi@gmail.com>
2020-08-13 12:02:41 -0300

    Comentando a parte não implementada do código

--- spec/routes/products/put_spec.rb
+++ spec/routes/products/put_spec.rb
@@ -1,24 +1,24 @@
-# dica, tente fazer o PUT no Insomia, depois no código!
-# dica, o PUT é um misto do Get + Post, ou seja tem Payload no Body e Id na Rota
+# # dica, tente fazer o PUT no Insomia, depois no código!
+# # dica, o PUT é um misto do Get + Post, ou seja tem Payload no Body e Id na Rota
 
-describe "PUT /products/:id" do
-  context "dado que eu tenho um produto" do
-    before(:all) do
-      # todo
-    end
+# describe "PUT /products/:id" do
+#   context "dado que eu tenho um produto" do
+#     before(:all) do
+#       # todo
+#     end
 
-    context "quando eu atualizo" do
-      before(:all) do
-        # todo
-      end
+#     context "quando eu atualizo" do
+#       before(:all) do
+#         # todo
+#       end
 
-      it "então deve retornar 204" do
-        expect(@resp.code).to eql 204
-      end
-    end
+#       it "então deve retornar 204" do
+#         expect(@resp.code).to eql 204
+#       end
+#     end
 
-    after (:all) do
-      # todo
-    end
-  end
-end
+#     after (:all) do
+#       # todo
+#     end
+#   end
+# end
